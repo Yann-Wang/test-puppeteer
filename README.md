414f8c5ea9fe0a112f71d5c0d288c84eec5b2e41
wangyanan <wangyanan@youzan.com>
2018-11-06 12:02:46 +0800

    feat: add an example about screenshot

---
+++ README.md
@@ -0,0 +1,7 @@
+### Chromium download url
+- https://download-chromium.appspot.com/
+- the directory: `../chrome-mac`
+
+### Environment
+- yarn v1.9.4
+- nvm 0.33.8
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+async function run() {
+  const browser = await puppeteer.launch({
+    executablePath: '../chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+  });
+  const page = await browser.newPage();
+  
+  await page.goto('https://github.com');
+  await page.screenshot({ path: 'screenshots/github.png' });
+  
+  browser.close();
+}
+
+run();
